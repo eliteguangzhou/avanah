ce457ab51e700e64c553c06ad76d346459c542b5
tidjean <tidjean@gmail.com>
2012-08-22 17:20:57 +0200

    condition

--- includes/languages/english/conditions.php
+++ includes/languages/english/conditions.php
@@ -14,7 +14,7 @@ define('NAVBAR_TITLE', 'Conditions of Use');
 define('HEADING_TITLE', 'Conditions of Use');
 
 define('TEXT_INFORMATION', '<div style="text-align:justify;">
-<br/><h1>PREAMBLE</h1><br/>
+<br/><h2>PREAMBLE</h2><br/>
  
 To help you in your choice of products best suited to your needs, our Pre-Sales-Service is available by mail at : contact@avanah-paris.com. 
 
@@ -22,48 +22,48 @@ If you want to follow the execution of your order or use your right of withdrawa
 
 By placing your order, you acknowledge that you have full legal capacity to enter into this sales agreement and you agree to abide by these terms and general conditions of sale. 
 
-<br/><h1>Article 1 - APPLICATION OF TERMS AND GENERAL CONDITIONS OF SALES</h1><br/>
+<br/><h2>Article 1 - APPLICATION OF TERMS AND GENERAL CONDITIONS OF SALES</h2><br/>
 
 These Terms and General Conditions of sales apply to all orders placed on the website Avanah-paris.com , exploited by REVOLUSITE SARL (SIRET 50127787500024) 25 Avenue de L\'Europe 92310 SEVRES, FRANCE ; and MERCURY CAPITAL CORPORATION LIMITED(business certificate 5069891) Flat B, 4th Floor, Haven Commercial Building, 6-8 Tsing Fung Street, North Point, Hong Kong.
 
 As Avanah-paris.com may be occasionally called upon to amend certain provisions of the Terms and General Conditions of Sales, they need to be reread before each order of products on the Site. Any change will be effective as of their online date and cannot be applied to orders placed prior to that date. Each order on the Site is governed by the Terms and General Conditions of Sales applicable at the date of such order. By validating your order, you unreservedly accept our General Conditions of Sales after reading them. 
 
-<br/><h1>Article 2 – ORDERS</h1><br/>
+<br/><h2>Article 2 – ORDERS</h2><br/>
  
-<h2>Ordering Process</h2>
+<br/><b>Ordering Process</b><br/>
 
 Clicking the "Purchase" button launches a product ordering process. One or more products can be added into the "cart". You can always change your order by updating your "cart". 
 
 After checking references and quantities of the desired products and updating the list of products in your "cart", you can submit your order by clicking the link "Purchase". Then simply provide the information requested and let yourself be guided by the website through the different necessary steps. 
 
-<h2>Necessary Information</h2> 
+<br/><b>Necessary Information</b><br/> 
 
 You must check and ensure the completeness and rightness of the information you provide when ordering, especially the delivery address. Avanah-paris.com could not be held responsible for any data entry errors and their consequences (e.g. delays or errors in delivery). In these cases, the costs of the return of the order would be the purchaser responsibility. 
 
 The information and personal information collected for distance selling are mandatory (phone number and email), being essential to process, deliver the orders and establish your invoice. A lack of any mandatory information will void the order. The client has a right to access, modify, rectify and delete any data that pertains to him, that he can fulfill by contacting us at Avanah-paris.com . 
 
-<h2>Order Processing </h2>
+<br/><b>Order Processing </b><br/>
 
 Orders are processed in the order in which they are actually received. Avanah-paris.com agrees to process all orders within the limits of available stocks. Deliveries are carried out subject to availability and following the order of arrival of orders and can be made totally or partially. 
 
-<h2>Order Entry </h2>
+<br/><b>Order Entry </b><br/>
 After placing your order, AVANAH-PARIS.COM will acknowledge it through a written email of Order Confirmation containing the summary of your order and the order reference. This email is sent to the email address that was used to login into your Customer Account. Note: If you do not receive the email mentioned above, we recommend that you contact our Customer Service. 
 
-<h2>Acceptance of the Order</h2>
+<br/><b>Acceptance of the Order</b><br/>
 Avanah-paris.com reserves the right not to accept your order for any legitimate reason, especially in cases where:
 The order does not comply with these Terms and General Conditions of Sales.
 One of your previous orders would not have been fully paid at due date.
 A dispute over payment of one of your previous orders is being processed.
 Several serious and corroborating elements pose a suspicion of fraud on your order.
 
-<br/><h1>Article 3 – PRICE</h1><br/>
+<br/><h2>Article 3 – PRICE</h2><br/>
  
 Our product offerings and prices are valid while they are visible on the website within the limits of available stocks. 
 
 The price of a product may fluctuate after it is posted on the Site. This fluctuation can be punctual as part of a promotional event whose duration can be limited in time or longer. The selling price given by Avanah-paris.com and displayed during promotional activities is a price well below normally charged price. 
 
 
-<br/><h1>Article 4 - PAYMENT</h1><br/>
+<br/><h2>Article 4 - PAYMENT</h2><br/>
  
 The following payment methods may be accepted by Avanah-paris.com :
 Bank transfer (TT)
@@ -72,34 +72,34 @@ Credit card
 PayPal: PayPal enables you to pay online without sharing your financial information during the transaction.
 Coupons: You can use vouchers for payment of all or part of your order. These vouchers can only be used once.
 
-<h2>Default of payment </h2>
+<br/><b>Default of payment </b><br/>
 Avanah-paris.com reserves the right to refuse a delivery or an order from a customer who would not have paid or fully paid a previous order or with who a payment dispute is under processing. 
 
-<h2>Security of Payments </h2>
+<br/><b>Security of Payments </b><br/>
 Payments made on our site are completely secure. Details of your payment card provided in your order are never transmitted unencrypted over the network : they are encrypted using SSL (Secure Socket Layer). 
 
-<br/><h1>Article 5 – DELIVERIES</h1><br/>
+<br/><h2>Article 5 – DELIVERIES</h2><br/>
  
 All delivery times mentioned are calculated in working days. The delivery date is calculated taking into account the time of preparation and shipment to which is added the delay of delivery of the carrier. 
 
 The products are delivered to the shipping address you provided during the order process. We ask you to be particularly watchful when receiving your order concerning the verification, detection and signalization of a possible anomaly. 
 
-<h2>Non Receipt of a Package </h2>
+<br/><b>Non Receipt of a Package </b><br/>
 In case of non-receipt of a parcel within the specified time, the Customer may contact the Customer Service which will determine the cause of delay and explain it to the Client. If necessary, Avanah-paris.com will open an investigation on the product delivery with the carrier concerned. The carrier sets himself the duration of the investigation of loss. 
 
 
-<br/><h1>Article 6 - FORCE MAJEURE AND LIABILITY</h1><br/>
+<br/><h2>Article 6 - FORCE MAJEURE AND LIABILITY</h2><br/>
  
 Avanah-paris.com cannot be held liable for breach of contract due to a case of force majeure. Avanah-paris.com cannot be held liable of a breach of contract arising from any Force Majeure occurrence not within the reasonable control of Avanah-paris.com including but not limited to disruption or total or partial strike of postal services and means of transport, communications (network failure etc.)., flood, fire, etc. ; whether affecting Avanah-paris.com or any of its suppliers or subcontractors, or for circumstances caused by reasons of law, regulations or actions or inactions of any government or other competent authority. 
 
 Avanah-paris.com aggregate liability to the Customer for damages under this Contract, and regardless of whether the claim for such damages is based in contract, tort, strict liability or otherwise, shall be limited to and under no circumstances exceed the purchase price of the Products, work or service provided under the Contract. Avanah-paris.com is under no circumstance liable for any indirect, consequential, special, punitive or incidental damage including, but not limited to, loss of profits or loss of business arising out of or in connection with this Contract. 
 
 
-<br/><h1>Article 7 - RETURN</h1><br/>
+<br/><h2>Article 7 - RETURN</h2><br/>
  
 To process your return request, thank you to mention your order reference number and your full contact details. In the case of a refund, the refund will be done in the month following receipt of the returned product. Products returned incomplete, improperly packaged, damaged or soiled will not be taken back. 
 
 
-<br/><h1>Article 8 – APPLICABLE LAW</h1><br/>
+<br/><h2>Article 8 – APPLICABLE LAW</h2><br/>
  These Terms and General Conditions of Sales are subject to the Online sales law of the country of delivery of the product.</div>');
 ?>
